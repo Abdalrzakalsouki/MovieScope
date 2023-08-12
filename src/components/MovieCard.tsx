038e9ceec278f47505112641015a00f44154194f
Abdulrazzak Alsssouki <abdulrzakalsssouki@gmail.com>
2023-08-12 20:09:05 +0200

    adding buttons for movie sliders

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,10 +1,40 @@
+/** @jsxImportSource @emotion/react */
+import { useState } from "react";
 import Typography from "@mui/material/Typography";
 import { Movie } from "../Interfaces/interface.ts";
 import MovieItem from "../components/MovieItem";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
+import { css } from "@emotion/react";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+
+const arrowLeft = css`
+  position: absolute;
+  content: "";
+  top: 50%;
+  font-size: 60px;
+  color: red;
+  transform: translate(-50%, -50%);
+  left: 10px;
+  cursor: pointer;
+`;
+
+const arrowRight = css`
+  position: absolute;
+  content: "";
+  top: 50%;
+  font-size: 60px;
+  color: red;
+  transform: translate(-50%, -50%);
+  right: 10px;
+  cursor: pointer;
+`;
+
+const movieCard = css`
+  background-color: #ffffff;
+`;
+
 const MovieCard = ({
   heading,
   movieList,
@@ -12,18 +42,29 @@ const MovieCard = ({
   heading: string;
   movieList: Movie;
 }) => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const endIndex = currentIndex + 5;
+  const movies = movieList.slice(currentIndex, endIndex);
+
+  const handleRightArrow = () => {
+    setCurrentIndex(currentIndex + 5);
+  };
+
+  const handleLeftArrow = () => {
+    if (currentIndex >= 5) setCurrentIndex(currentIndex - 5);
+  };
   return (
-    <Container>
+    <Container sx={{ position: "relative" }}>
       <Typography sx={{ paddingBlock: "2rem" }} variant="h6">
         {heading}
       </Typography>
-      <Grid container spacing={2}>
-        {movieList.map((movie) => (
+      <Grid container spacing={2} css={movieCard}>
+        {movies.map((movie) => (
           <MovieItem movie={movie} imageSize="250px" gridSpace={[10, 6, 3]} />
         ))}
       </Grid>
-      <ChevronRightIcon />
-      <ChevronLeftIcon />
+      <ChevronLeftIcon css={arrowLeft} onClick={() => handleLeftArrow()} />
+      <ChevronRightIcon css={arrowRight} onClick={() => handleRightArrow()} />
     </Container>
   );
 };
--- src/views/Movies.tsx
+++ src/views/Movies.tsx
@@ -12,14 +12,6 @@ const Movies = () => {
   const upcomingEndpoint = import.meta.env.VITE_UPCOMING;
   const genersEndpoint = import.meta.env.VITE_GENRES;
 
-  const sliceArr = (
-    arr: Array<object>,
-    strIndex: number = 0,
-    endIndex: number = 4
-  ) => {
-    if (arr) return arr.slice(strIndex, endIndex);
-  };
-
   const {
     data: popular,
     loading: popularLoader,
@@ -44,20 +36,17 @@ const Movies = () => {
     error: genersError,
   } = useFetch(genersEndpoint, credit);
 
-  const popularList = sliceArr(popular?.results);
-  const topRatedList = sliceArr(topRated?.results);
-  const upcomingList = sliceArr(upcoming?.results);
   return (
     <div>
       <Navbar />
       {popular && (
-        <MovieCard heading="Popular movies" movieList={popularList} />
+        <MovieCard heading="Popular movies" movieList={popular.results} />
       )}
       {topRated && (
-        <MovieCard heading="Popular movies" movieList={topRatedList} />
+        <MovieCard heading="Popular movies" movieList={topRated.results} />
       )}
       {upcoming && (
-        <MovieCard heading="Popular movies" movieList={upcomingList} />
+        <MovieCard heading="Popular movies" movieList={upcoming.results} />
       )}
     </div>
   );
