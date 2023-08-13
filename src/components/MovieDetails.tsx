954a354de49653c4358f96a6f939049008a31a15
Abdulrazzak Alsssouki <abdulrzakalsssouki@gmail.com>
2023-08-13 09:16:57 +0200

    improve styling

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,29 +1,70 @@
+/** @jsxImportSource @emotion/react */
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { MovieItemProps } from "../Interfaces/interface.tsx";
+import { css } from "@emotion/react";
+import Navbar from "../views/Navbar.tsx";
+
+const movieContainer = css`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 2rem 20px 20px 20px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const moviePoster = css`
+  max-width: 100%;
+  height: auto;
+`;
+
+const infoContainer = css`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const title = css`
+  color: #db0000;
+  font-size: 2rem;
+  padding-bottom: 1rem;
+  font-weight: bold;
+`;
+
+const overviewInfo = css`
+  max-width: 75%;
+  padding-bottom: 1rem;
+`;
 const MovieDetails: React.FC<MovieItemProps> = ({ movie }) => {
   return (
     <div>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={4}>
-          <img
-            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-            alt={movie.title}
-            style={{ width: "100%", height: "auto" }}
-          />
+      <Navbar />
+
+      <div css={movieContainer}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={4}>
+            <img
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+              alt={movie.title}
+              css={moviePoster}
+            />
+          </Grid>
+          <Grid item xs={12} sm={8} css={infoContainer}>
+            <Typography css={title} variant="h1">
+              {movie.title}
+            </Typography>
+            <Typography css={overviewInfo}>{movie.overview}</Typography>
+            <Typography>Adult: {movie.adult ? "Yes" : "No"}</Typography>
+            <Typography>Language: {movie.original_language}</Typography>
+            <Typography>Release Date: {movie.release_date}</Typography>
+            <Typography>Popularity: {movie.popularity}</Typography>
+            <Typography>Rating: {movie.vote_average}</Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={8}>
-          <Typography variant="h5">{movie.title}</Typography>
-          <Typography variant="h6">{movie.original_title}</Typography>
-          <Typography>{movie.overview}</Typography>
-          <Typography>{movie.adult}</Typography>
-          <Typography>{movie.original_language}</Typography>
-          <Typography>{movie.release_date}</Typography>
-          <Typography>{movie.popularity}</Typography>
-          <Typography>{movie.vote_average}</Typography>
-        </Grid>
-      </Grid>
-      Test
+      </div>
     </div>
   );
 };
